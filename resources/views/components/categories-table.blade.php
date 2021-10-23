fe645f01ea234e9259db32720a8aae8166dc6232
issam <issamyayou@gmail.com>
2021-10-23 12:27:30 +0100

    updated the tabe styles

--- resources/views/components/categories-table.blade.php
+++ resources/views/components/categories-table.blade.php
@@ -1,21 +1,21 @@
 <div>
-    <table class="table-auto">
+    <table class="w-full whitespace-nowrap">
         <thead>
-          <tr>
-            <th>Category ID</th>
-            <th>Category Name</th>
-            <th>Picture</th>
-            <th>Dishes Number</th>
+          <tr class="text-left font-bold">
+            <th class="border px-6 py-4">Category ID</th>
+            <th class="border px-6 py-4">Category Name</th>
+            <th class="border px-6 py-4">Picture</th>
+            <th class="border px-6 py-4">Dishes Number</th>
           </tr>
         </thead>
         <tbody>
 
             @foreach ($categories as $category)
               <tr>
-                <td>{{ $category ->id }}</td>
-                <td>{{ $category ->name }}</td>
-                <td> <a href="{{ $category->pic_url() }}"> {{ $category ->picture_name }} </a></td>
-                <td>{{ $category ->dishes->count() }}</td>
+                <td class="border px-6 py-4">{{ $category ->id }}</td>
+                <td class="border px-6 py-4">{{ $category ->name }}</td>
+                <td class="border px-6 py-4"> <a href="{{ $category->pic_url() }}"> {{ $category ->picture_name }} </a></td>
+                <td class="border px-6 py-4">{{ $category ->dishes->count() }}</td>
               </tr>
             @endforeach
 
--- resources/views/components/dishes-table.blade.php
+++ resources/views/components/dishes-table.blade.php
@@ -1,25 +1,25 @@
 <div>
-    <table class="table-auto">
+    <table class="w-full whitespace-nowrap">
         <thead>
-          <tr>
-            <th>Dish ID</th>
-            <th>Dish Name</th>
-            <th>Category Name</th>
-            <th>Picture</th>
-            <th>Ingredient Number</th>
-            <th>Preparation Steps Number</th>
+          <tr class="text-left font-bold">
+            <th class="border px-3 py-2">Dish ID</th>
+            <th class="border px-3 py-2">Dish Name</th>
+            <th class="border px-3 py-2">Category Name</th>
+            <th class="border px-3 py-2">Ingredient Number</th>
+            <th class="border px-3 py-2">Preparation Steps Number</th>
+            <th class="border px-3 py-2 max-w-5">Picture</th>
           </tr>
         </thead>
         <tbody>
 
             @foreach ($dishes as $dish)
               <tr>
-                <td>{{ $dish->id }}</td>
-                <td>{{ $dish->name }}</td>
-                <td>{{ $dish->category?->name }}</td>
-                <td> <a href="{{ $dish->pic_url() }}"> {{ $dish->picture_name }} </a></td>
-                <td>{{ $dish->ingredients?->count() }}</td>
-                <td>{{ $dish->preparation_steps->count() }}</td>
+                <td class="border px-3 py-2">{{ $dish->id }}</td>
+                <td class="border px-3 py-2">{{ $dish->name }}</td>
+                <td class="border px-3 py-2">{{ $dish->category?->name }}</td>
+                <td class="border px-3 py-2">{{ $dish->ingredients?->count() }}</td>
+                <td class="border px-3 py-2">{{ $dish->preparation_steps->count() }}</td>
+                <td class="border px-3 py-2 max-w-5"> <a href="{{ $dish->pic_url() }}"> {{ $dish->name }} - Pic </a></td>
               </tr>
             @endforeach
 
