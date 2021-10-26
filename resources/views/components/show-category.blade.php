eb7865ad1a84dda8baca100ff44701acbcdbc704
issam <issamyayou@gmail.com>
2021-10-26 19:55:59 +0100

    fix the width and height of img

--- resources/views/components/show-category.blade.php
+++ resources/views/components/show-category.blade.php
@@ -2,7 +2,7 @@
     <h1 class="text-red-700"> {{ __('Category Name')}} : {{ $category->name }} </h1>
     <h3 class="text-red-700"> {{ __('Category Picture')}} </h3>
     <div>
-        <img src="{{ asset('storage/images/'. $category->picture_name) }}" alt="category name">
+        <img src="{{ asset('storage/images/'. $category->picture_name) }}" width="300" height="300" alt="category name">
     </div>
 
 </div>
